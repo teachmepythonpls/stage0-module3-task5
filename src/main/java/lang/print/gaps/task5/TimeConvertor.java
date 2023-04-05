3ff78171c2d6a5428bcc461e4a03fd4f6f7a38d3
edvinasvencevicius <edviz.nzn@gmail.com>
2023-04-05 20:17:13 +0300

    sss

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+        public void convert(float minutes){
+            float seconds = 60 * minutes;
+            System.out.println(seconds);
+        }
     }
-}
